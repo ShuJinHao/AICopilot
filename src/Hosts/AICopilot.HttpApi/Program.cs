00103814b8d6fb99983598278c5410c232b1945e
Jin Hao <jinhao_liang2022@163.com>
2026-01-22 09:50:26 +0800

    项目初始化

---
+++ src/Hosts/AICopilot.HttpApi/Program.cs
@@ -0,0 +1,37 @@
+using AICopilot.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.AddServiceDefaults();
+
+builder.AddNpgsqlDbContext<AiCopilotDbContext>("ai-copilot");
+
+// Add services to the container.
+
+builder.Services.AddInfrastructures(builder.Configuration);
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapDefaultEndpoints();
+
+app.Run();
---
+++ src/Infrastructure/AICopilot.EntityFrameworkCore/AiCopilotDbContext.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AICopilot.EntityFrameworkCore;
+
+public class AiCopilotDbContext(DbContextOptions<AiCopilotDbContext> options) : IdentityDbContext(options);
