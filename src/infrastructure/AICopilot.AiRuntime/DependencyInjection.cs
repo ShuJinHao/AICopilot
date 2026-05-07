d0f82dccc4cf5b563872e659c4fafc67fffab2e0
Jin Hao <jinhao_liang2022@163.com>
2026-05-07 11:34:40 +0800

    aicopilot: configure model provider reliability

--- src/infrastructure/AICopilot.AiRuntime/DependencyInjection.cs
+++ src/infrastructure/AICopilot.AiRuntime/DependencyInjection.cs
@@ -8,10 +8,13 @@ public static class DependencyInjection
 {
     public static void AddAiRuntime(this IHostApplicationBuilder builder)
     {
+        builder.Services
+            .AddOptions<ModelProviderReliabilityOptions>()
+            .BindConfiguration(ModelProviderReliabilityOptions.SectionName);
         builder.Services.AddSingleton<IModelProviderHealth, AlwaysHealthyModelProviderHealth>();
         builder.Services.AddSingleton<IModelFallbackPolicy, DefaultModelFallbackPolicy>();
         builder.Services.AddSingleton<IModelCircuitBreaker, InMemoryModelCircuitBreaker>();
-        builder.Services.AddSingleton<IModelCostBudgetPolicy, NoOpModelCostBudgetPolicy>();
+        builder.Services.AddSingleton<IModelCostBudgetPolicy, ConfiguredModelCostBudgetPolicy>();
         builder.Services.AddSingleton<IAgentRuntimeFactory, AgentRuntimeFactory>();
     }
 }
